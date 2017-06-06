21d659380ccac007ffcd47eb3f3c440112d3fc9f
Sari Stissi <sestissi16@earlham.edu>
2017-06-06 09:48:28 -0400

    Added second input and both inputs change the header

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value
+    heading.textContent = f.personName.value + f.elements[1].value
+    /*ev.preventDefault()
+    let f = ev.target document.getElementById('personForm')
+    const heading = document.querySelector('h1')
+    for(let i = 0; i < f.length; i++){
+        heading.textContent += ' ' + f.elements[i].value + ' '
+    }*/
 }
+
 
 personForm.addEventListener('submit', handleSubmit)
