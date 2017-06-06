17d8293950d033a3fc025a8d6351ed960b3eb8d2
Sari Stissi <sestissi16@earlham.edu>
2017-06-06 13:29:56 -0400

    fix issue with the name personColor so it works like personName

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value + ', ' + f.elements[1].value
-    const textToAdd = document.createTextNode(f.personName.value + "'s favorite color is " + f.elements[1].value)
+    heading.textContent = f.personName.value + ', ' + f.personColor.value
+    const textToAdd = document.createTextNode(f.personName.value + "'s favorite color is " + f.personColor.value)
     const pElement = document.getElementById('newP')
     pElement.appendChild(textToAdd)
-    document.getElementById('newP').style.color = f.elements[1].value.toLowerCase()
+    document.getElementById('newP').style.color = f.personColor.value.toLowerCase()
 }
 
 
