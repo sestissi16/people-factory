a3920ae3e9505da8ff7c638c9ff74e1c0c49ed6c
Sari Stissi <sestissi16@earlham.edu>
2017-06-06 10:05:35 -0400

    Add empty paragraph that receives values from inputs

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value + f.elements[1].value
-    /*ev.preventDefault()
-    let f = ev.target document.getElementById('personForm')
-    const heading = document.querySelector('h1')
-    for(let i = 0; i < f.length; i++){
-        heading.textContent += ' ' + f.elements[i].value + ' '
-    }*/
+    heading.textContent = f.personName.value + ', ' + f.elements[1].value
+    const textToAdd = document.createTextNode(f.personName.value + "'s favorite color is " + f.elements[1].value)
+    const pElement = document.getElementById('newP')
+    pElement.appendChild(textToAdd)
 }
+
 
 
 personForm.addEventListener('submit', handleSubmit)
