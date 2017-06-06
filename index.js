99dbc8ca81631b44b24be5b2f31991fb6a7d608c
Sari Stissi <sestissi16@earlham.edu>
2017-06-06 14:06:15 -0400

    Add name to details

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const personForm = document.querySelector('#personForm')
 function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
-    const heading = document.querySelector('h1')
+    const details = document.querySelector('#details')
+    const name = f.personName.value
+
+    details.innerHTML +='<strong>' + name + '</strong>'
+    /*const heading = document.querySelector('h1')
     heading.textContent = f.personName.value + ', ' + f.personColor.value
     const textToAdd = document.createTextNode(f.personName.value + "'s favorite color is " + f.personColor.value)
     const pElement = document.getElementById('newP')
     pElement.appendChild(textToAdd)
-    document.getElementById('newP').style.color = f.personColor.value.toLowerCase()
+    document.getElementById('newP').style.color = f.personColor.value.toLowerCase()*/
 }
 
 
